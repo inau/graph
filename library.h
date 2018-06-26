da5d7b4ddda394d49ce74cda8e03c9b629c91d71
Ivan Naumovski <inau@itu.dk>
2018-06-26 17:02:43 +0200

    started refactoring for interop

--- library.h
+++ library.h
@@ -2,37 +2,121 @@
 #define GRAPH_LIBRARY_H
 
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 namespace Graph {
+    extern "C++" {
+    class UnionFind {
+    protected:
+        int *Rank, *Parent;
+        const int vertices;
 
-    template<class T>
-    class Node {
+        bool IsRoot(int v) { return Parent[v] == v; };
+
+        int FindRoot(int v) {
+            int r = v;
+            while (!IsRoot(r)) {
+                r = Parent[r];
+            }
+            //compress
+            Parent[v] = r;
+
+            return r;
+        }
+
+        void AttachComponentToRoot(int v, int root) {
+            Parent[v] = root;
+            Rank[root] += Rank[v];
+        }
+
+    public:
+        UnionFind(int vertex_count) : vertices(vertex_count) {
+            Rank = new int[vertices];
+            std::fill_n(Rank, vertices, 1);
+            Parent = new int[vertices];
+            for (int i = 0; i < vertices; ++i) {
+                Parent[i] = i;
+            }
+        }
+
+        ~UnionFind() {
+            delete (Rank);
+            delete (Parent);
+        }
+
+        void Union(const int v0, const int v1) {
+            int r;
+            if (Rank[v0] >= Rank[v1]) {
+                r = FindRoot(v0);
+                AttachComponentToRoot(v1, r);
+            } else {
+                r = FindRoot(v1);
+                AttachComponentToRoot(v0, r);
+            }
+        }
+
+        int Find(const int v) {
+            return FindRoot(v);
+        }
+    };
+    };
+
+    template <class T>
+    struct Edge {
+        int vertex_from, vertex_to;
+        T edge_weight;
+        //Edge() : vertex_from(0), vertex_to(0), edge_weight(0) {};
+        //Edge(int v0, int v1, T weight) : vertex_from(v0), vertex_to(v1), edge_weight(weight) {};
+        bool operator<(Edge other) const {
+            return edge_weight > other.edge_weight;
+        }
+    };
+
+    extern "C" {
+        typedef Edge<int> intEdge;
+    };
+
+
+    class Vertex {
     private:
-        T value;
         std::vector<int> adjacent;
 
     public:
-        void AddAdjacentVertex(int id) { this->adjacent.push_back(id); };
-        std::vector<int> GetAdjacentVertices() { return this->adjacent; };
+        void AddEdge(int e_id) { this->adjacent.push_back(e_id); };
 
+        std::vector<int> GetAdjacentVertices() { return this->adjacent; };
     };
 
-    /** Graph Base Class **/
+    /**
+     * Graph Base Class
+     **/
     template<class T>
     class Graph {
     protected:
-        Node<T> *nodes;
+        Vertex *nodes;
+        std::vector<Edge<T>> edges;
+
         const int sz;
+        void _AddEdge(int v1, int v2, T weight, bool biDirectional = false) {
+            Edge<T> e{v1,v2,weight};
+            edges.push_back(e);
+            nodes[v1].AddEdge(edges.size()-1);
+            if( biDirectional )
+                nodes[v2].AddEdge(edges.size()-1);
+        }
 
     public:
-        Graph(int vertices) : sz(vertices) { nodes = new Node<T>[sz]; };
+        Graph(int vertices) : sz(vertices) {
+            nodes = new Vertex[sz];
+        };
 
-        Node<T> *GetVertex(int v) { return &nodes[v]; };
-
-        void AddEdge(int v1, int v2) {
-            nodes[v1].AddAdjacentVertex(v2);
-            nodes[v2].AddAdjacentVertex(v1);
+        void AddEdge(int v1, int v2, T weight, bool biDirectional = false) {
+            _AddEdge(v1,v2,weight,biDirectional);
         }
+
+        Vertex *GetVertex(int v) { return &nodes[v]; };
+        std::vector<Edge<T>> GetEdges() { return edges; };
 
         int GraphSize() { return sz; }
     };
@@ -52,20 +136,71 @@ namespace Graph {
         int ColSize() { return col_sz; }
         int RowSize() { return row_sz; }
 
-        void AddEdgeTo(int x1, int y1, int x2, int y2) {
+        void AddEdge(int x1, int y1, int x2, int y2, T weight,  bool biDirectional = false) {
             int v1 = mapper_index(x1, y1), v2 = mapper_index(x2,y2);
-            this->AddEdge(v1, v2);
+            this->_AddEdge(v1, v2, weight, biDirectional);
         }
 
-        Node<T> *GetVertexAt(int col, int row) {
+
+        Vertex *GetVertexAt(int col, int row) {
             return &this->nodes[ mapper_index(col, row) ];
         };
 
     };
 
-    class MinimumSpanningTree {
+    template <class T>
+    std::vector<Edge<T>> minimum_spanning_tree(Graph<T> &g ) {
+        std::vector<Edge<T>> mst;
 
-    };
+        //sort edges by weight
+        std::vector<Edge<T>> sortedEdges = g.GetEdges();
+        std::sort(sortedEdges.begin(), sortedEdges.end() );
+
+        //UnionFind structure
+        UnionFind uf( g.GraphSize() );
+
+        //add smallest edge
+        Edge<T> *e;
+        e = &sortedEdges[0];
+        uf.Union( e->vertex_from , e->vertex_to );
+
+        for (int i = 1; i < sortedEdges.size(); i++) {
+            e = &sortedEdges[i];
+
+            //find parent affiliation
+            int pi = uf.Find( e->vertex_from );
+            int pu = uf.Find( e->vertex_to );
+
+            //different components
+            if( pi != pu ) {
+                uf.Union(e->vertex_from, e->vertex_to);
+                mst.push_back(*e);
+            }
+
+            //if edges added reach V-1 we have a edge for every pair of nodes
+            if( !(mst.size() < g.GraphSize()) ) break;
+        }
+        return mst;
+    }
+
+
+    template <class T>
+    void PrintGraph(Graph<T> &g) {
+        std::cout << "Vertex outdegree" << "\n" ;
+        for (int i = 0; i < g.GraphSize(); ++i) {
+            std::cout << g.GetVertex(i)->GetAdjacentVertices().size() << " " ;
+        }
+    }
+
+    template <class T>
+    void PrintGraph(GridGraph<T> &g) {
+        std::cout << "Vertex outdegree" << "\n" ;
+        for (int col = 0; col < g.ColSize(); ++col) {
+            for (int row = 0; row < g.RowSize(); ++row) {
+                std::cout << g.GetVertexAt(col, row)->GetAdjacentVertices().size() << " " ;
+            }
+            std::cout << "\n" ;
+        }
+    }
 }
-
 #endif
--- main.cpp
+++ main.cpp
@@ -7,23 +7,32 @@
 #include "library.h"
 
 int main(int argc, char** argv) {
-    Graph::Graph<int> g(9);
+    Graph::GridGraph<int> g(3,3);
     Graph::GridGraph<int> g2(3,3);
 
     std::cout << g.GraphSize() << std::endl;
-    std::cout << g2.GraphSize() << std::endl;
+   // std::cout << g2.GraphSize() << std::endl;
 
     for(int y = 0; y < g2.RowSize() ; y++) {
         for(int x = 0; x < g2.ColSize() ; x++) {
-            if( y < g2.RowSize()-1 )
-                g2.AddEdgeTo(x, y, x, y+1);
+            if( y < g2.RowSize()-1 ) {
+                g.AddEdge(x, y, x, y+1, 1, true);
+                g2.AddEdge(x, y, x, y+1, 1);
+            }
+
             if(x == g2.ColSize()-1) continue;
-                g2.AddEdgeTo(x, y, x+1, y);
+            g.AddEdge(x, y, x+1, y, 1, true);
+            g2.AddEdge(x, y, x+1, y, 1);
         }
     }
 
-    for(int i = 0; i < g2.GraphSize() ; i++) {
-        std::cout << g2.GetVertex(i)->GetAdjacentVertices().size() << " ";
+    Graph::PrintGraph(g);
+    //Graph::PrintGraph(g2);
+
+    auto mst = Graph::minimum_spanning_tree(g);
+
+    for (auto it = mst.begin(); it != mst.end(); ++it) {
+        std::cout << "(" << it.operator->()->vertex_from << ", " << it.operator->()->vertex_to << ") ";
     }
 
     /*******************************************
